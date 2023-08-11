036623640cb3d0f12eeda3f81b223cf1055870f7
gabriell nascimento <gabriell.nascimento@deliveryhero.com>
2023-08-11 18:19:55 +0200

    feat: fix paths

--- custom/kube.sh
+++ custom/kube.sh
@@ -5,7 +5,7 @@ show_kube() { # save this module in a file with the name <module_name>.sh
   local index=$1 # this variable is used by the module loader in order to know the position of this module 
   local icon="$(get_tmux_option "@catppuccin_kube_icon" "󱃾")"
   local color="$(get_tmux_option "@catppuccin_kube_color" "#66ccff")"
-  local text="$(get_tmux_option "@catppuccin_kube_text" "$(/bin/bash ${SCRIPT_DIR}/../extra/kube.tmux)")"
+  local text="$(get_tmux_option "@catppuccin_kube_text" "$(bash ${SCRIPT_DIR}/../plugins/kube.tmux)")"
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
 
